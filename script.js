55a859f202fccc9fd3e800530e2ecb472f978117
Matheus Mota <matheusmgf2010@gmail.com>
2024-03-18 16:26:15 -0300

    aplicação completa

---
+++ script.js
@@ -0,0 +1,32 @@
+let voltar = document.getElementById('voltar');
+let proximo = document.getElementById('proximo');
+let imagens = document.getElementById('imagens');
+
+let fila_de_imagens = ['./imagens/chrono.jpg', './imagens/inuyasha.jpg' , './imagens/ippo.png' , './imagens/tenchi.jpg' , './imagens/tenjhotenge.jpg' , './imagens/yuyuhakusho.jpg'];
+
+let i = 0;
+
+function proximo_fila(){
+  if(i > 5){
+    i = 0;
+    imagens.src = fila_de_imagens[i];
+  }else{
+    imagens.src = fila_de_imagens[i];
+    i++ ;
+    }
+}
+
+function voltar_fila(){
+    if(i < 0){
+      i = 5;
+      imagens.src = fila_de_imagens[i];
+    }else{
+      imagens.src = fila_de_imagens[i];
+      i--;
+    }
+    
+}
+
+
+proximo.addEventListener('click' , proximo_fila)
+voltar.addEventListener('click' , voltar_fila)
